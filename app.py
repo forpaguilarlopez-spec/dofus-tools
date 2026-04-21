2250736d7d47b24325c42021d652b8092463aa4d
Pablo Aguilar <forpaguilarlopez@gmail.com>
2026-04-21 02:08:14 +0200

    feat: leer y escribir datos JSON, ruta toggle

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+import json
+import os
 
 app = Flask(__name__)
 
+DATA_FILE = 'data.json'
+
+def leer_datos():
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def guardar_datos(datos):
+    with open(DATA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(datos, f, indent=2, ensure_ascii=False)
+
 @app.route('/')
 def inicio():
-    return 'Hola desde DOFUS Tools'
+    datos = leer_datos()
+    return str(datos)
+
+@app.route('/toggle/<int:id>', methods=['POST'])
+def toggle(id):
+    datos = leer_datos()
+    for tarea in datos['tasks']:
+        if tarea['id'] == id:
+            tarea['hecho'] = not tarea['hecho']
+    guardar_datos(datos)
+    return {'ok': True}
 
 if __name__ == '__main__':
     app.run(debug=True)
